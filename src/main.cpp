e70755facc90aa97d5a08ebfed9d1f13b359a47c
canadacoin <canadacoin2014@gmail.com>
2014-03-06 17:16:40 -0500

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2002,7 +2002,7 @@ bool LoadBlockIndex(bool fAllowNew)
     
         
         // Genesis block
-        const char* pszTimestamp = "Traditionally one puts something timely here coinciding with the epoch";
+        const char* pszTimestamp = "Traditionally one does not make frivolous cryptocurrencies for fun and profit";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
